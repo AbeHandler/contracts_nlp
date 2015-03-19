ad501486f26b0d57b4d4638dcc269b5965b19c03
AbeHandler <abram.handler@gmail.com>
2015-03-19 11:29:07 -0500

    adding parser reader filertering

--- parser/parser_reader.py
+++ parser/parser_reader.py
@@ -6,6 +6,11 @@ data_file = "".join([p for p in open("parsed.json")])
 
 hits = re.findall("{[^}]+}", data_file)
 
+agreement_amounts = []
+
 for h in hits:
     h = ast.literal_eval(h)
-    print h.keys()
+    if "agreement_amount" in h.keys():
+        agreement_amounts.append(h)
+
+print agreement_amounts
