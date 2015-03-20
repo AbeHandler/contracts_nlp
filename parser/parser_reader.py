c32c39c81e1d505e08bdbb6f56c14b6c5cee18d5
AbeHandler <abram.handler@gmail.com>
2015-03-20 16:27:25 -0500

    adding parser reader and more labeled data

--- parser/parser_reader.py
+++ parser/parser_reader.py
@@ -2,7 +2,7 @@ import json
 import ast
 import re
 from colorama import Fore, Back, Style
-
+import csv
 
 def filter_hits(hits, filter_term):
     hits = [ast.literal_eval(h) for h in hits]
@@ -13,6 +13,11 @@ def filter_hits(hits, filter_term):
 def color_me_red(input_text, color_this):
     return input_text.replace(color_this, Fore.RED + color_this + Fore.RESET)
 
+def write_new(line):
+    with open('corrected.csv', 'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([line])
+
 
 data_file = "".join([p for p in open("parsed.json")])
 
@@ -20,5 +25,12 @@ hits = re.findall("{[^}]+}", data_file)
 
 hits = filter_hits(hits, "agreement_amount")
 
+counter = 0
+
 for h in hits:
+    counter += 1
+    print str(counter) + " of " + str(len(hits))
     print "{} | {}".format(color_me_red(h['agreement_amount'][0], h['agreement_amount'][0]), color_me_red(h['original'].strip(), h['agreement_amount'][0]))
+    var = raw_input("Correct (c) or Not (n)?")
+    if var == "n":
+        write_new(h['original'])
