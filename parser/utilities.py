3d725db753fc07ed4bc96280ac4de10286d00439
akh2103 <akh2103@gmail.com>
2015-03-23 10:27:32 -0500

    adding utils

--- parser/utilities.py
+++ parser/utilities.py
@@ -8,8 +8,8 @@ def get_page_with_phrase(doc_cloud_id, phrase):
     doc = client.documents.get(doc_cloud_id)
     for p in range(1,doc.pages + 1):
         text = doc.get_page_text(p)
-        #if phrase in doc.get_page_text(p):
-        #    return p
+        if phrase in doc.get_page_text(p):
+            return p
     return 1
 
 
